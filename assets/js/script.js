83c8b2f58efd9fbcb4e8214047b72ab77aeff139
saif <saifalis@axioned.com>
2022-09-22 18:09:51 +0530

    feedback function completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
-/* Author: 
+// feedback function
+var feedbackList = document.querySelectorAll('.feedback-list');
+var feedback = document.querySelectorAll('.feedback');
+var feedText = document.querySelector('.feedback-text');
+var reviewBtn = document.querySelector('.review-btn');
+var feedresult = "";
+feedbackList.forEach(function (list ,index) {
+  list.addEventListener ('click', function () {
+    var activeFeed = document.querySelector('.active-list');
+    activeFeed.classList.remove('active-list');
+    list.classList.add('active-list');
+    feedresult = feedback[index].innerHTML;
+    feedText.innerHTML = feedresult;
+  })
+});
 
-*/
+reviewBtn.addEventListener('click' , showReview);
 
-
+function showReview() {
+  var feedbackCategory = document.querySelector('.feedback-category');
+  var showResult = document.querySelector('.result');
+  feedbackCategory.style.display = "none";
+  showResult.style.display = "block";
+}
 
 
 
